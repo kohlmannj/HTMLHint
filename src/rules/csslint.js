b2c34005508a1a2b6a5ada0af634a13e1961db79
Joseph Kohlmann <joseph.kohlmann@nytimes.com>
2018-04-25 03:42:22 -0400

    Feature: Implement style attribute CSS linting

--- src/rules/csslint.js
+++ src/rules/csslint.js
@@ -6,7 +6,7 @@ export default {
     init(parser, reporter, options) {
         parser.addListener("cdata", event => {
             if (event.tagName.toLowerCase() === "style") {
-                if (options !== undefined) {
+                if (typeof options === "object" && options !== null) {
                     const styleLine = event.line - 1,
                         styleCol = event.col - 1;
                     try {
@@ -23,6 +23,33 @@ export default {
                         });
                     } catch (e) {} // eslint-disable-line no-empty
                 }
+            }
+        });
+
+        parser.addListener("tagstart", event => {
+            const attrs = event.attrs,
+                tagCol = event.col + event.tagName.length + 1,
+                attr = attrs.find(a => a.name.toLowerCase() === "style");
+            const value = attr ? attr.value : undefined;
+            if (typeof value === "string" && typeof options === "object" && options !== null) {
+                try {
+                    const fakeSelectorStart = ".style {",
+                        fakeSelectorEnd = "}";
+                    const messages = CSSLint.verify(
+                        `${fakeSelectorStart}${value}${fakeSelectorEnd}`,
+                        options
+                    ).messages;
+                    messages.forEach(error => {
+                        const line = error.line;
+                        reporter[error.type === "warning" ? "warn" : "error"](
+                            `[${error.rule.id}] ${error.message}`,
+                            event.line + line - (line === 1 ? 2 : 1),
+                            (line === 1 ? tagCol - fakeSelectorStart.length : 0) + error.col,
+                            this,
+                            error.evidence
+                        );
+                    });
+                } catch (e) {} // eslint-disable-line no-empty
             }
         });
     },
